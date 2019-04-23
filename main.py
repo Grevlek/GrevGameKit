664b484b60c6c883af0eac64eb451461646328bd
Grevlek <grevlek@gmail.com>
2019-04-23 01:16:55 -0500

    End of Night 1
    End of Night 1

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+
+from modPlayerCharacter import *
+
 def main():
     print("Main File")
+    p1 = PlayerCharacter()
+    p2 = PlayerCharacter(1500)
+    p3 = PlayerCharacter(6500)
+    print(p1)
+    print(p2)
+    print(p3)
+    
 
 main()
--- modPlayerCharacter.py
+++ modPlayerCharacter.py
@@ -2,8 +2,22 @@
 
 class PlayerCharacter:
 
-    def __init__(self,startXP):
-        self._charStaringXP = startXP
+    def __init__(self,startXP=0):
+        self._charStartingXP = startXP
+        self._charXP = self._charStartingXP
+        self._charLevel = self.calcLevel()
+
+    def __str__(self):
+        return "object is a level " + str(self._charLevel)
+
+    def calcLevel(self):
+        totalXP = self._charXP
+        levelResult = 1
+        for number in range(20):
+            if totalXP >= ( ( number + 1 ) * 1000 ):
+                print(number)
+                levelResult = number
+        return levelResult        
 
     
         
